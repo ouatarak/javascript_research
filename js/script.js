8f4f385f7ef34843a0464661c67d1d251a9bff66
ouatarak <ouatarak@yahoo.fr>
2020-04-05 17:06:03 -0600

    added filtering

--- js/script.js
+++ js/script.js
@@ -1,8 +1,36 @@
-const numbers = [1,2,3,4,5];
-/*numbers.reverse();
-console.log(numbers);*/
-const doubledNumber = [];
+// Learning Functional Programing with JS ES6+//
+
+/*const numbers = [1,2,3,4,5];
+numbers.reverse();
+console.log(numbers);
+const doubledNumbers = [];
 for (let i= 0; i< numbers.length; i++) {
-    doubledNumber.push(numbers[i] * 2);
+    doubledNumbers.push(numbers[i] * 2);
     console.log(doubledNumber);
 }
+fonctinning 
+const numbers = [1,2,3,4,5];
+const double = x => x * 2;
+const doubledNumbers = numbers.map (double);
+console.log(doubledNumbers);
+
+const timeItself = []
+const numbers = [1,2,3,4,5];
+const multiplyByitself = x => x * x;
+for (let i= 0; i< numbers.length; i++); {
+    const timeItself = numbers.map (multiplyByitself);
+    console.log(timeItself);
+}
+*/
+
+//filter//
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const isEven = x => x % 2 === 0;
+const evenNumbers = numbers.filter(isEven);
+console.log(evenNumbers);
+
+
+        
+
+ 
