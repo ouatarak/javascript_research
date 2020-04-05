13258c6747b4fc7168a309a5fd4c4d1229f5ef61
ouatarak <ouatarak@yahoo.fr>
2020-04-04 21:20:12 -0600

    added mapping

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const numbers = [1,2,3,4,5];
+/*numbers.reverse();
+console.log(numbers);*/
+const doubledNumber = [];
+for (let i= 0; i< numbers.length; i++) {
+    doubledNumber.push(numbers[i] * 2);
+    console.log(doubledNumber);
+}
