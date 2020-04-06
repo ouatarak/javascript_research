b0520bc5ca2a6ad045378cdfcf9b3c8b7203a1f5
ouatarak <ouatarak@yahoo.fr>
2020-04-05 18:28:00 -0600

    added slice/sort

--- js/script.js
+++ js/script.js
@@ -41,7 +41,7 @@ const isLongerThan5 = word => word.length > 5;
 const longWords = words.filter(isLongerThan5);
 console.log(longWords);
 
-*/
+
 //Every/Some
  
 const formValues = [
@@ -54,3 +54,38 @@ const isNotEmpty = string => !! string;
 const allFieldsFilled = formValues.every(isNotEmpty);
 console.log(allFieldsFilled);
 
+
+// Slice & Sort //
+
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(numbers.slice(2, 8));
+
+
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(numbers.slice());
+
+
+
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(numbers.slice().reverse());
+
+const mixedUpNumbers = [10, 2, 4, 3, 7, 5, 8, 9, 1, 6];
+const ascending = (a, b) => {
+    if (a < b) return -1;
+    if (a > b) return 1;
+    return 0;
+}
+
+const sortedNumbers = mixedUpNumbers.slice().sort(ascending);
+console.log(sortedNumbers);
+*/
+
+const mixedUpNumbers = [10, 2, 4, 3, 7, 5, 8, 9, 1, 6];
+const descending = (a, b) => {
+    if (a > b) return -1;
+    if (a < b) return 1;
+    return 0;
+}
+
+const sortedNumbers = mixedUpNumbers.slice().sort(descending);
+console.log(sortedNumbers);
